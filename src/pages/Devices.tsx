0da41aca1e0b48faa978232f63df4bf19525310c
hotaru84 <c11t11w@msn.com>
2023-07-02 17:20:16 +0900

    list

--- src/pages/Devices.tsx
+++ src/pages/Devices.tsx
@@ -1,6 +1,7 @@
-import { Wrap, Container, useDisclosure, Image, Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, Center, Stack } from "@chakra-ui/react";
+import { Wrap, Container, useDisclosure, Image, Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, Center, Text, VStack } from "@chakra-ui/react";
 import React, {  } from "react";
 import { DeviceCard, DeviceInfo } from "../component/DeviceCard";
+import { motion } from "framer-motion";
 const devices:DeviceInfo[] = [
   {
     name:"test",
@@ -31,26 +32,34 @@ const devices:DeviceInfo[] = [
 export default function Index() {
   const {isOpen,onOpen,onClose} = useDisclosure();
   return (
-    <Container height={"100vh"} maxW={"full"} overflowY={"hidden"} >
+    <Container  maxW={"full"} overflowY={"auto"} height={"100vh"}>
+      <Text>Device type</Text>
       <Wrap 
         justify={"center"}
-        mt={4}
         spacing={4}
-        height="100%"
-        width={'full'}
-        overflowY={'auto'}
+        py={2}
         shouldWrapChildren
       >
         {devices.map((d,index) => (
           <DeviceCard info={d} key={index} onClick={onOpen}/>
         ))}
       </Wrap>
-      
+      <Text>Device type2</Text>
+      <Wrap 
+        justify={"center"}
+        spacing={4}
+        py={2}
+        shouldWrapChildren
+      >
+        {devices.map((d,index) => (
+          <DeviceCard info={d} key={index} onClick={onOpen}/>
+        ))}
+      </Wrap>
      <Drawer
         isOpen={isOpen}
         onClose={onClose}
         placement='right'
-        size={'md'}
+        size={['full','md']}
         colorScheme='gray'
       >
         <DrawerContent>
